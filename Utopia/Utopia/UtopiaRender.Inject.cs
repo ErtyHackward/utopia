af8e527048b5e4b6cf507b65b72a8183bb600ef7
s33m3 <fabian.ceressia@gmail.com>
2011-08-19 00:33:55 +0200

    [Client] Small test with Ninject

---
+++ Utopia/Utopia/UtopiaRender.Inject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+using Utopia.Worlds.GameClocks;
+
+namespace Utopia
+{
+    public partial class UtopiaRender
+    {
+        private void InjectionContainersCreation()
+        {
+            using (IKernel kernel = new StandardKernel())
+            {
+                kernel.Bind<IClock>()
+                      .To<WorldClock>()
+                      .InSingletonScope()
+                      .WithConstructorArgument("Game", this)
+                      .WithConstructorArgument("clockSpeed", 480)
+                      .WithConstructorArgument("startTime", (float)Math.PI * 1f);
+            }
+        }
+    }
+}
