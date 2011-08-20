0189acd4c51ecf6abddbbee84a9879d027f45ff9
Vladislav Pozdnyakov <hackward@gmail.com>
2011-08-20 18:48:38 +0700

    - server scheduler added. It can be used by Services to run periodically events

---
+++ Utopia/Utopia.Server/Managers/ScheduleCallType.cs
@@ -0,0 +1,18 @@
+namespace Utopia.Server.Managers
+{
+    public enum ScheduleCallType
+    {
+        /// <summary>
+        /// Task will be executed once and then deleted from manager
+        /// </summary>
+        Once,
+        /// <summary>
+        /// Task will be executed periodically
+        /// </summary>
+        Periodic,
+        /// <summary>
+        /// Task will be executed each day at time specified
+        /// </summary>
+        SpecialTime
+    }
+}
