4a6da041f5379ced2dd5e4f4378ba7313a2dffbb
Vladislav Pozdnyakov <hackward@gmail.com>
2011-08-24 15:44:40 +0700

    - added NetworkMessageFactory to create message by id   remeber that using it will create overhead in boxing/unboxing the structure   to be able to pass it by interface - ServerConnection now buffers all messages to fire event in main   thread. Use FetchPendingMessages() to process all pending messages - ToolBarSlot now stores only position of item in the inventory

---
+++ Utopia/Utopia.Net/Messages/NetworkMessageFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Utopia.Net.Interfaces;
+
+namespace Utopia.Net.Messages
+{
+    /// <summary>
+    /// Creates network message class from binary stream by message Id.
+    /// </summary>
+    public class NetworkMessageFactory
+    {
+        static NetworkMessageFactory()
+        {
+            Instance = new NetworkMessageFactory();
+        }
+
+        /// <summary>
+        /// Gets factory instance
+        /// </summary>
+        public static NetworkMessageFactory Instance { get; private set; }
+
+        /// <summary>
+        /// Creates network message
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public IBinaryMessage ReadMessage(MessageTypes id, BinaryReader reader)
+        {
+            switch (id)
+            {
+                case MessageTypes.BlockChange: return BlockChangeMessage.Read(reader);
+                case MessageTypes.Chat: return ChatMessage.Read(reader);
+                case MessageTypes.ChunkData: return ChunkDataMessage.Read(reader);
+                case MessageTypes.DateTime: return DateTimeMessage.Read(reader);
+                case MessageTypes.Error: return ErrorMessage.Read(reader);
+                case MessageTypes.GameInformation: return GameInformationMessage.Read(reader);
+                case MessageTypes.GetChunks: return GetChunksMessage.Read(reader);
+                case MessageTypes.Login: return LoginMessage.Read(reader);
+                case MessageTypes.LoginResult: return LoginResultMessage.Read(reader);
+                case MessageTypes.PlayerDirection: return PlayerDirectionMessage.Read(reader);
+                case MessageTypes.PlayerIn: return PlayerInMessage.Read(reader);
+                case MessageTypes.PlayerOut: return PlayerOutMessage.Read(reader);
+                case MessageTypes.PlayerPosition: return PlayerPositionMessage.Read(reader);
+                default:
+                    throw new ArgumentException("");
+            }
+        }
+    }
+}
--- Utopia/Utopia.Shared/Chunks/Entities/Inventory/ToolbarSlot.cs
+++ Utopia/Utopia.Shared/Chunks/Entities/Inventory/ToolbarSlot.cs
@@ -1,49 +1,85 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Utopia.Shared.Structs;
 
 namespace Utopia.Shared.Chunks.Entities.Inventory
 {
     /// <summary>
-    /// Represents a toolbar slot, that available to contain a pair of items
+    /// Represents a toolbar slot, that available to contain a link to pair of slots into the inventory
     /// </summary>
     public class ToolbarSlot : ContainedSlot
     {
-        public Slot Left { get; set; }
-        public Slot Right { get; set; }
+        /// <summary>
+        /// Inventory position of the left subslot
+        /// </summary>
+        public Location2<byte>? Left { get; set; }
+
+        /// <summary>
+        /// Inventory position of the right subslot
+        /// </summary>
+        public Location2<byte>? Right { get; set; }
 
         // overriding items and do whole writing by ourself
 
         public override void Save(BinaryWriter writer)
         {
+            if (Left == null && Right == null) 
+                throw new InvalidOperationException("Unable to save ToolBar slot because it is empty. Empty slot should not be saved.");
+
+            // saving toolbar slot position
+
             writer.Write(GridPosition.X);
             writer.Write(GridPosition.Z);
 
-            if(Left != null)
-                Left.Save(writer);
-            else 
-                WriteEmpty(writer);
+            // we do not going to write empty subslots 
+            // storage layout byte 0 - only left, 1 - only right, 2 - both slots
+            byte layout = (Left != null && Right != null) ? (byte)2 : (Left != null ? (byte)0 : (byte)1);
 
-            if(Right != null)
-                Right.Save(writer);
-            else
-                WriteEmpty(writer);
-            
+            writer.Write(layout);
+
+            // saving child slots location
+            if (Left != null)
+            {
+                writer.Write(Left.Value.X);
+                writer.Write(Left.Value.Z);
+            }
+
+            if (Right != null)
+            {
+                writer.Write(Right.Value.X);
+                writer.Write(Right.Value.Z);
+            }
         }
 
         public override void Load(BinaryReader reader)
         {
+            // reading toolbar slot position
             Location2<byte> location;
             location.X = reader.ReadByte();
             location.Z = reader.ReadByte();
             GridPosition = location;
 
-            Left.Load(reader);
-            if (Left.IsEmpty)
-                Left = null;
+            //read layout byte
+            var layout = reader.ReadByte();
 
-            Right.Load(reader);
-            if (Right.IsEmpty)
-                Right = null;
+            if (layout == 0 || layout == 2)
+            {
+                // left slot position
+                Location2<byte> leftPos;
+                leftPos.X = reader.ReadByte();
+                leftPos.Z = reader.ReadByte();
+                Left = leftPos;
+            }
+
+            if (layout == 1 || layout == 2)
+            {
+                // left slot position
+                Location2<byte> rightPos;
+                rightPos.X = reader.ReadByte();
+                rightPos.Z = reader.ReadByte();
+                Right = rightPos;
+            }
+
         }
     }
 }
