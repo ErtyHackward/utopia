a445afce4bbab4e55faf9ba47486756d1beb7dda
enomi <enomi@localhost>
2011-08-31 13:02:32 -0400

    EntityRenderer VoxelItem work in progress - bug : doesnt render

--- Utopia/Utopia/Entities/EntityRenderer.cs
+++ Utopia/Utopia/Entities/EntityRenderer.cs
@@ -2,30 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using S33M3Engines;
+using S33M3Engines.Cameras;
 using S33M3Engines.D3D;
+using S33M3Engines.D3D.Effects.Basics;
+using S33M3Engines.InputHandler;
+using S33M3Engines.Struct.Vertex;
+using S33M3Engines.WorldFocus;
+using SharpDX;
+using Utopia.Entities.Voxel;
 
 namespace Utopia.Entities
 {
     public class EntityRenderer : GameComponent
     {
         #region Private variables
+
         #endregion
 
         #region Public variables/properties
+
         public List<IEntity> Entities = new List<IEntity>();
+        private HLSLVertexPositionColor _itemEffect;
+        private readonly D3DEngine _d3DEngine;
+        private readonly WorldFocusManager _worldFocusManager;
+        private readonly CameraManager _camManager;
+        private readonly InputHandlerManager _inputHandler; //temporary, for spawning item
+        private VoxelMeshFactory _voxelItem;
+
         #endregion
 
-        public EntityRenderer()
+        public EntityRenderer(D3DEngine d3DEngine, WorldFocusManager worldFocusManager, CameraManager camManager,
+                              InputHandlerManager inputHandler)
         {
+            _d3DEngine = d3DEngine;
+            _inputHandler = inputHandler;
+            _camManager = camManager;
+            _worldFocusManager = worldFocusManager;
         }
 
         #region Public Methods
+
         public override void Initialize()
         {
         }
 
         public override void LoadContent()
         {
+            _itemEffect = new HLSLVertexPositionColor(_d3DEngine, @"D3D/Effects/Basics/VertexPositionColor.hlsl",
+                                                      VertexPositionColor.VertexDeclaration);
+
             for (int i = 0; i < Entities.Count; i++)
             {
                 Entities[i].LoadContent();
@@ -38,6 +65,8 @@ namespace Utopia.Entities
             {
                 Entities[i].UnloadContent();
             }
+
+            if (_itemEffect != null) _itemEffect.Dispose();
         }
 
         public override void Update(ref GameTime TimeSpend)
@@ -46,6 +75,15 @@ namespace Utopia.Entities
             {
                 Entities[i].Update(ref TimeSpend);
             }
+
+
+            if (_inputHandler.IsKeyPressed(Keys.Insert))
+            {
+                _voxelItem = new VoxelMeshFactory(_d3DEngine);
+                _voxelItem.Blocks = new byte[16,16,16];
+                _voxelItem.RandomFill(5);
+                _voxelItem.GenCubesFaces();
+             }
         }
 
         public override void Interpolation(ref double interpolation_hd, ref float interpolation_ld)
@@ -61,6 +99,34 @@ namespace Utopia.Entities
             for (int i = 0; i < Entities.Count; i++)
             {
                 Entities[i].DrawDepth0();
+            }
+
+          
+        }
+
+        private void DrawItem()
+        {
+            Matrix worldFocused = Matrix.Identity;
+            Matrix world =Matrix.Translation((float)_camManager.ActiveCamera.WorldPosition.X,
+                                              -(float) _camManager.ActiveCamera.WorldPosition.Y,
+                                              (float) _camManager.ActiveCamera.WorldPosition.Z);
+            
+            _worldFocusManager.CenterOnFocus(ref world, ref world);
+
+            _itemEffect.Begin();
+            _itemEffect.CBPerDraw.Values.World = Matrix.Transpose(worldFocused);
+            _itemEffect.CBPerDraw.IsDirty = true;
+            _itemEffect.CBPerFrame.Values.View = Matrix.Transpose(_camManager.ActiveCamera.View);
+            _itemEffect.CBPerFrame.Values.Projection = Matrix.Transpose(_camManager.ActiveCamera.Projection3D);
+            _itemEffect.CBPerFrame.IsDirty = true;
+            _itemEffect.Apply();
+
+            _voxelItem.SendMeshToGraphicCard();
+            if (_voxelItem.VertexBuffer != null)
+            {
+                _voxelItem.VertexBuffer.SetToDevice(0);
+
+                _d3DEngine.Context.Draw(_voxelItem.VertexBuffer.VertexCount, 0);
             }
         }
 
@@ -78,10 +144,14 @@ namespace Utopia.Entities
             {
                 Entities[i].DrawDepth2();
             }
+            if (_voxelItem != null)
+                DrawItem();
         }
+
         #endregion
 
         #region Private Methods
-        #endregion        
+
+        #endregion
     }
 }
---
+++ Utopia/Utopia/Entities/Voxel/ColorLookup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utopia.Shared.Structs;
+
+namespace Utopia.Entities.Voxel
+{
+    /// <summary>
+    /// Generates a lookup array for 8 bits color
+    /// No need to optimize it's purely static !
+    /// </summary>
+    public static class ColorLookup
+    {
+        public static readonly Color[] Colours;
+
+        /// <summary>
+        /// Palette of 64 nice colours
+        /// </summary>
+        private static readonly string[] _palette = new string[] {
+            "ffffffff","ffdbdbdb","ffb7b7b7","ff929292","ff6e6e6e","ff494949","ff252525","ff000000",
+            "ffff0000","ffff2400","ffff4800","ffff6d00","ffff9100","ffffb600","ffffda00","ffffff00",
+            "ffffff00","ffdbed00","ffb7db00","ff92c900","ff6eb700","ff49a500","ff259300","ff008000",
+            "ff008000","ff006e24","ff005c48","ff004a6d","ff003791","ff0025b6","ff0013da","ff0000ff",
+            "ff0000ff","ff2400db","ff4800b7","ff6d0092","ff91006e","ffb60049","ffda0025","ffff0000",
+            "fff9decc","fff5d7c4","fff1d0bb","ffecc9b3","ffe8c2aa","ffe3bba2","ffdfb499","ffdaad90",
+            "ffffffff","ffdbdbff","ffb7b7ff","ff9292ff","ff6e6eff","ff4949ff","ff2525ff","ff0000ff",
+            "ffc08e6c","ff9a7156","ff735440","ff4c3729","ffff0000","ffff0055","ffff00aa","ffff00ff"
+        };
+
+        static ColorLookup()
+        {
+            Colours = new Color[_palette.Length];
+            for (int i = 0; i < _palette.Length; i++)
+            {
+                Colours[i] = HexStringToColor(_palette[i]);
+            }
+        }
+
+
+        private static Color HexStringToColor(string hexColor)
+        {
+
+            string a = hexColor.Substring(0, 2);
+            string r = hexColor.Substring(2, 2);
+            string g = hexColor.Substring(4, 2);
+            string b = hexColor.Substring(6, 2);
+            int ai
+               = Int32.Parse(a, System.Globalization.NumberStyles.HexNumber);
+            int ri
+               = Int32.Parse(r, System.Globalization.NumberStyles.HexNumber);
+            int gi
+               = Int32.Parse(g, System.Globalization.NumberStyles.HexNumber);
+            int bi
+               = Int32.Parse(b, System.Globalization.NumberStyles.HexNumber);
+            return new Color(ri, gi, bi, ai);
+        }
+
+    }
+}
