b84b11ac109a5651cdcef2c330d5bd82f8358d0d
Vladislav Pozdnyakov <hackward@gmail.com>
2011-09-06 07:47:11 +0700

    - temp work on moisture

--- Utils/Utopia.MapGenerator/frmMain.Designer.cs
+++ Utils/Utopia.MapGenerator/frmMain.Designer.cs
@@ -47,6 +47,7 @@
             this.centerElevationCheck = new System.Windows.Forms.CheckBox();
             this.moisturizeCheck = new System.Windows.Forms.CheckBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.voronoiPolyNumeric)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.voronoiSeedNumeric)).BeginInit();
@@ -87,7 +88,7 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(155, 13);
+            this.pictureBox1.Location = new System.Drawing.Point(155, 19);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(724, 509);
             this.pictureBox1.TabIndex = 2;
@@ -279,11 +280,21 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(152, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "label6";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(891, 534);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.moisturizeCheck);
             this.Controls.Add(this.centerElevationCheck);
@@ -337,6 +348,7 @@
         private System.Windows.Forms.CheckBox centerElevationCheck;
         private System.Windows.Forms.CheckBox moisturizeCheck;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
     }
 }
 
