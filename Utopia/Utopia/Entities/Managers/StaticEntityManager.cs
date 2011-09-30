f40444611b7147ebb745e02ed1a4569135dd1644
s33m3 <fabian.ceressia@gmail.com>
2011-09-30 22:36:52 +0200

    Sprites Load test. Can you tell me how this is running on your computer (CPU impact ? FPS ? ...)
    Not optimize at all.
    CleanUp DB before startup, it should placed nearly at every surface block a grass flower.
    Its easy heavily the Geometry shader

--- Utopia/Utopia/Entities/Managers/StaticEntityManager.cs
+++ Utopia/Utopia/Entities/Managers/StaticEntityManager.cs
@@ -31,8 +31,10 @@ namespace Utopia.Entities.Managers
         private D3DEngine _d3dEngine;
         private WorldFocusManager _worldFocusManager;
         private IStaticSpriteEntityRenderer _spriteRenderer;
-        private List<VisualSpriteEntity> _spriteEntitiesToRender;
+        private VisualSpriteEntity[] _spriteEntitiesToRender;
+        private int _spriteEntitiesToRenderNbr;
         private IWorldChunks _worldChunks;
+        private readonly int _spriteMaxSize = 50000;
         #endregion
 
         #region Public variables/properties
@@ -46,105 +48,55 @@ namespace Utopia.Entities.Managers
             _d3dEngine = d3dEngine;
             _worldFocusManager = worldFocusManager;
             _spriteRenderer = spriteRenderer;
-            _spriteEntitiesToRender = new List<VisualSpriteEntity>();
+            _spriteEntitiesToRender = new VisualSpriteEntity[_spriteMaxSize];
             _spriteRenderer.SpriteEntities = _spriteEntitiesToRender;
             _worldChunks = worldChunks;
         }
 
         public override void Initialize()
         {
-            //TextureAnimationOffset.Value = 0.00f;
-            //TextureAnimationOffset.ValuePrev = 0.00f;
         }
 
         public override void LoadContent()
         {
-            //_effectPointSprite = new HLSLPointSprite3D(_d3dEngine, @"D3D\Effects\Basics\PointSprite3D.hlsl", VertexPointSprite.VertexDeclaration);
-            //CreateBuffer();
-            //ArrayTexture.CreateTexture2DFromFiles(_d3dEngine.Device, @"Textures/Sprites/", @"sp*.png", FilterFlags.Point, "ArrayTexture_WorldChunk", out _srv);
-            //_effectPointSprite.SamplerDiffuse.Value = StatesRepository.GetSamplerState(GameDXStates.DXStates.Samplers.UVClamp_MinMagMipPoint);
-            //_effectPointSprite.DiffuseTexture.Value = _srv;
         }
 
         public override void Dispose()
         {
-            //_effectPointSprite.Dispose();
         }
         #region Private Methods
-
-        //private void CreateBuffer()
-        //{
-        //    VertexPointSprite[] vertices = new VertexPointSprite[] { new VertexPointSprite((byte)0, new ByteVector4(1, 0, 0, 0)) };
-        //    _vb = new VertexBuffer<VertexPointSprite>(_d3dEngine, vertices.Length, VertexPointSprite.VertexDeclaration, PrimitiveTopology.PointList, "PointSprite3D");
-        //    _vb.SetData(vertices);
-        //}
         #endregion
 
         #region Public Methods
-        //private float _windpower;
-        //float offset = 0.01f;
-        //FTSValue<float> TextureAnimationOffset = new FTSValue<float>();
         public override void Update(ref GameTime timeSpent)
         {
-            _spriteEntitiesToRender.Clear();
+            _spriteEntitiesToRenderNbr = -1;
 
-            //VisualChunk chunk;
-            ////Check inside the visible chunks (Not visible culled) the statics entities that needs to be rendered
-            //for (int i = 0; i < _worldChunks.Chunks.Length; i++)
-            //{
-            //    chunk = _worldChunks.Chunks[i];
-            //    if (chunk.isFrustumCulled == false)
-            //    {
-            //        foreach (var entity in chunk.VisualSpriteEntities )
-            //        {
-            //            _spriteEntitiesToRender.Add(entity);
-            //        }
-            //    }
-            //}
-
-            _spriteEntitiesToRender.Add(new VisualSpriteEntity(new Utopia.Shared.Chunks.Entities.Concrete.Collectible.Grass() { Position = new Vector3D(0, 100, 0) }));
-
+            VisualChunk chunk;
+            //Check inside the visible chunks (Not visible culled) the statics entities that needs to be rendered
+            for (int i = 0; i < _worldChunks.Chunks.Length; i++)
+            {
+                chunk = _worldChunks.Chunks[i];
+                if (chunk.isFrustumCulled == false && chunk.State == ChunkState.DisplayInSyncWithMeshes)
+                {
+                    for (int j = 0; j < chunk.VisualSpriteEntities.Count; j++)
+                    {
+                        _spriteEntitiesToRenderNbr++;
+                        _spriteEntitiesToRender[_spriteEntitiesToRenderNbr] = chunk.VisualSpriteEntities[j];
+                        if (_spriteEntitiesToRenderNbr >= _spriteMaxSize) break;
+                    }
+                }
+            }
+            _spriteRenderer.SpriteEntitiesNbr = _spriteEntitiesToRenderNbr;
             _spriteRenderer.Update(ref timeSpent);
-
-            //TextureAnimationOffset.BackUpValue();
-            //TextureAnimationOffset.Value += offset;
-            //if (TextureAnimationOffset.Value > 0.3)
-            //{
-            //    offset *= -1;
-            //}
-            //if (TextureAnimationOffset.Value < -0.3)
-            //{
-            //    offset *= -1;
-            //}
         }
         public override void Interpolation(ref double interpolationHd, ref float interpolationLd)
         {
-            //TextureAnimationOffset.ValueInterp = MathHelper.Lerp(TextureAnimationOffset.ValuePrev, TextureAnimationOffset.Value, interpolationLd);
         }
 
         public override void Draw(int Index)
         {
-
             _spriteRenderer.Draw(Index);
-
-            //StatesRepository.ApplyStates(GameDXStates.DXStates.Rasters.CullNone, GameDXStates.DXStates.Blenders.Disabled, GameDXStates.DXStates.DepthStencils.DepthEnabled);
-
-            //_effectPointSprite.Begin();
-
-            //_effectPointSprite.CBPerFrame.Values.ViewProjection = Matrix.Transpose(_camManager.ActiveCamera.ViewProjection3D_focused);
-            //_effectPointSprite.CBPerFrame.Values.WindPower = TextureAnimationOffset.ValueInterp;
-            //_effectPointSprite.CBPerFrame.IsDirty = true;
-
-            //Matrix test = Matrix.Translation(0,100,0);
-            //_worldFocusManager.CenterTranslationMatrixOnFocus(ref test, ref test);
-
-            //_effectPointSprite.CBPerDraw.Values.World = Matrix.Transpose(test);
-            //_effectPointSprite.CBPerDraw.IsDirty = true;
-
-            //_effectPointSprite.Apply();
-
-            //_vb.SetToDevice(0);
-            //_d3dEngine.Context.Draw(_vb.VertexCount, 0);
         }
 
         #endregion
