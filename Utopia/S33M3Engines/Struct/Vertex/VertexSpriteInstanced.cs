6fe4c9aa84459aca1b8c2992849f3d9198e986aa
s33m3 <s33m3@localhost>
2011-10-13 13:09:24 +0200

    - Add possibility to have transparent fonts.

--- Utopia/S33M3Engines/Struct/Vertex/VertexSpriteInstanced.cs
+++ Utopia/S33M3Engines/Struct/Vertex/VertexSpriteInstanced.cs
@@ -10,6 +10,7 @@ using System.Globalization;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
 using RectangleF = System.Drawing.RectangleF;
+using Utopia.Shared.Structs;
 
 namespace S33M3Engines.Struct.Vertex
 {
@@ -17,12 +18,12 @@ namespace S33M3Engines.Struct.Vertex
     public struct VertexSpriteInstanced : IVertexType
     {
         public Matrix Tranform;
-        public Color4 Color;
+        public ByteColor Color;
         public RectangleF SourceRect;
         public int TextureArrayIndex;
         public static readonly VertexDeclaration VertexDeclaration;
 
-        public VertexSpriteInstanced(Matrix tranform, Color4 color, RectangleF sourceRect)
+        public VertexSpriteInstanced(Matrix tranform, ByteColor color, RectangleF sourceRect)
         {
             this.Tranform = tranform;
             this.Color = color;
@@ -30,7 +31,7 @@ namespace S33M3Engines.Struct.Vertex
             TextureArrayIndex = 0;
         }
 
-        public VertexSpriteInstanced(Matrix tranform, Color4 color)
+        public VertexSpriteInstanced(Matrix tranform, ByteColor color)
         {
             this.Tranform = tranform;
             this.Color = color;
@@ -38,7 +39,7 @@ namespace S33M3Engines.Struct.Vertex
             TextureArrayIndex = 0;
         }
 
-        public VertexSpriteInstanced(Matrix tranform, Color4 color,int textureArrayIndex )
+        public VertexSpriteInstanced(Matrix tranform, ByteColor color, int textureArrayIndex)
         {
             this.Tranform = tranform;
             this.Color = color;
@@ -65,13 +66,13 @@ namespace S33M3Engines.Struct.Vertex
             InputElement[] elements = new InputElement[] { 
                                                             new InputElement("POSITION", 0, Format.R32G32_Float, 0, 0, InputClassification.PerVertexData, 0), 
                                                             new InputElement("TEXCOORD", 0, Format.R32G32_Float, InputElement.AppendAligned, 0, InputClassification.PerVertexData, 0), //Instead of 8 use AppendAligned
-                                                            new InputElement("TRANSFORM", 0, Format.R32G32B32A32_Float, 0, 1, InputClassification.PerInstanceData, 1),
-                                                            new InputElement("TRANSFORM", 1, Format.R32G32B32A32_Float, 16, 1, InputClassification.PerInstanceData, 1),
-                                                            new InputElement("TRANSFORM", 2, Format.R32G32B32A32_Float, 32, 1, InputClassification.PerInstanceData, 1),
-                                                            new InputElement("TRANSFORM", 3, Format.R32G32B32A32_Float, 48, 1, InputClassification.PerInstanceData, 1),
-                                                            new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 64, 1, InputClassification.PerInstanceData, 1),
-                                                            new InputElement("SOURCERECT", 0, Format.R32G32B32A32_Float, 80, 1, InputClassification.PerInstanceData, 1),
-                                                            new InputElement("TEXINDEX", 0, Format.R32_UInt, 96, 1, InputClassification.PerInstanceData, 1)
+                                                            new InputElement("TRANSFORM", 0, Format.R32G32B32A32_Float, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1),
+                                                            new InputElement("TRANSFORM", 1, Format.R32G32B32A32_Float, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1),
+                                                            new InputElement("TRANSFORM", 2, Format.R32G32B32A32_Float, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1),
+                                                            new InputElement("TRANSFORM", 3, Format.R32G32B32A32_Float, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1),
+                                                            new InputElement("COLOR", 0, Format.R8G8B8A8_UNorm, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1),
+                                                            new InputElement("SOURCERECT", 0, Format.R32G32B32A32_Float, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1),
+                                                            new InputElement("TEXINDEX", 0, Format.R32_UInt, InputElement.AppendAligned, 1, InputClassification.PerInstanceData, 1)
                                                                                                             
             };
 
