d4c320fb9efbcb82f97f129e899e17a92418a5a2
Vladislav Pozdnyakov <hackward@gmail.com>
2011-12-11 15:12:18 +0700

    - [web] register, email confirm

--- Web/UtopiaApi/UtopiaApi/Models/RegisterModel.cs
+++ Web/UtopiaApi/UtopiaApi/Models/RegisterModel.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using UtopiaApi.Library;
 
 namespace UtopiaApi.Models
 {
     public class RegisterModel
     {
+        [Required]
+        [Email]
         public string Email { get; set; }
+
+        [Required]
+        [Password]
         public string Password { get; set; }
+
+        [Required]
+        [Password]
         public string RepeatPassword { get; set; }
+
         public Language Language { get; set; }
     }
 
