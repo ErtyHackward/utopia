9e162777b7110f2a9f7d7cb259a110847ccb8fc1
Vladislav Pozdnyakov <hackward@gmail.com>
2011-12-13 20:55:48 +0700

    - fixed server startup issue

--- Utopia/Utopia.Server/ServerSettings.cs
+++ Utopia/Utopia.Server/ServerSettings.cs
@@ -15,30 +15,45 @@ namespace Utopia.Server
         /// </summary>
         [DefaultValue(60000)]
         public int CleanUpInterval { get; set; }
+
         /// <summary>
         /// Interval between save operation executions. All modified chunks will be saved into database. 
         /// </summary>
         [DefaultValue(30000)]
         public int SaveInterval { get; set; }
+
         /// <summary>
         /// Defines maximum allowed chunk view range
         /// </summary>
         [DefaultValue(20)]
         public int MaxViewRangeChunks { get; set; }
+
         /// <summary>
         /// Defines how long server will store unused chunks
         /// </summary>
         [DefaultValue(60)]
         public int ChunkLiveTimeMinutes { get; set; }
+
         /// <summary>
         /// Port to listen on
         /// </summary>
         [DefaultValue(4815)]
         public int ServerPort { get; set; }
+
         /// <summary>
         /// Database file location
         /// </summary>
         public string DatabasePath { get; set; }
+
+        /// <summary>
+        /// Server global name
+        /// </summary>
+        public string ServerName { get; set; }
+
+        /// <summary>
+        /// Full address string of the server
+        /// </summary>
+        public string ServerAddress { get; set; }
 
         public void Initialize()
         {
