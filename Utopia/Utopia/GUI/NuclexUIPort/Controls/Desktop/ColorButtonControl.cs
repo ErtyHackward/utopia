81e7a5598ee9279b006c8ea2ac68ab87b51545d9
Vladislav Pozdnyakov <hackward@gmail.com>
2012-01-09 20:55:12 +0700

    - editor: tools progress, color palette

---
+++ Utopia/Utopia/GUI/NuclexUIPort/Controls/Desktop/ColorButtonControl.cs
@@ -0,0 +1,9 @@
+﻿using Utopia.Shared.Structs;
+
+namespace Utopia.GUI.NuclexUIPort.Controls.Desktop
+{
+    public class ColorButtonControl : StickyButtonControl
+    {
+        public Color Color { get; set; }
+    }
+}
