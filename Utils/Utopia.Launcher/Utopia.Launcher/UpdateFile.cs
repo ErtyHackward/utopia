bfdc8aeda6d4c8c8567ff6968da1d811314e26a2
Vladislav Pozdnyakov <hackward@gmail.com>
2012-02-02 21:58:21 +0700

    - auto update system WIP

---
+++ Utils/Utopia.Launcher/Utopia.Launcher/UpdateFile.cs
@@ -0,0 +1,20 @@
+﻿namespace Utopia.Launcher
+{
+    public class UpdateFile
+    {
+        /// <summary>
+        /// Relative file path
+        /// </summary>
+        public string FilePath;
+
+        /// <summary>
+        /// File md5 hash
+        /// </summary>
+        public string Md5Hash;
+
+        /// <summary>
+        /// network place for new file location
+        /// </summary>
+        public string DownloadUrl;
+    }
+}
---
+++ Utils/Utopia.Launcher/Utopia.Launcher/UpdateManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utopia.Launcher
+{
+    public class UpdateManager
+    {
+        private Dictionary<string, UpdateFile> _localFiles = new Dictionary<string, UpdateFile>();
+        private int _hashed = 0;
+        private string _localPath;
+
+        /// <summary>
+        /// Collects all files and takes a md5 hash
+        /// </summary>
+        public void CollectFiles()
+        {
+            _localFiles.Clear();
+            _hashed = 0;
+            _localPath = Application.StartupPath;
+            CollectDirectory(_localPath);
+            HashFiles();
+        }
+
+        private void CollectDirectory(string path)
+        {
+            foreach (var dirPath in Directory.EnumerateDirectories(path))
+            {
+                CollectDirectory(dirPath);
+            }
+
+            foreach (var filePath in Directory.EnumerateFiles(path))
+            {
+                var relativePath = filePath.Remove(0, _localPath.Length);
+
+                var item = new UpdateFile { FilePath = relativePath };
+
+                _localFiles.Add(relativePath, item);
+            }
+        }
+
+        private void HashFiles()
+        {
+            var md5Hasher = MD5.Create();
+            foreach (var pair in _localFiles)
+            {
+                byte[] hash;
+                using (var fs = File.OpenRead(pair.Key))
+                {
+                    hash = md5Hasher.ComputeHash(fs);
+                }
+
+                var sb = new StringBuilder();
+
+                foreach (var b in hash)
+                    sb.Append(b.ToString("x2").ToLower());
+
+                pair.Value.Md5Hash = sb.ToString();
+            }
+        }
+
+
+
+    }
+}
