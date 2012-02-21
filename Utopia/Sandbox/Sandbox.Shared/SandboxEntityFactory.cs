c800a110985eb0ffd57db44c3f44df4e5a7d853b
Vladislav Pozdnyakov <hackward@gmail.com>
2012-02-21 18:24:08 +0700

    - finished renaming

---
+++ Utopia/Sandbox/Sandbox.Shared/SandboxEntityFactory.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox.Shared.Items;
+using Sandbox.Shared.Tools;
+using Utopia.Shared.Entities;
+using Utopia.Shared.Interfaces;
+
+namespace Sandbox.Shared
+{
+    public class SandboxEntityFactory : EntityFactory
+    {
+        public SandboxEntityFactory(ILandscapeManager2D landscapeManager)
+            : base(landscapeManager)
+        {
+        }
+
+        protected override Entity CreateCustomEntity(ushort classId)
+        {
+            switch (classId)
+            {
+                case SandboxEntityClassId.Annihilator: return new Annihilator();
+                case SandboxEntityClassId.Shovel: return new Shovel();
+                case SandboxEntityClassId.GoldCoin: return new GoldCoin();
+                case SandboxEntityClassId.Editor: return new Editor();
+                case SandboxEntityClassId.Carver: return new Carver();
+                case SandboxEntityClassId.Torch: return new Torch();
+            }
+
+            return null;
+        }
+
+    }
+}
