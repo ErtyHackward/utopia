336dd89cef78dde53c3845ccdb471d28dbe31fad
s33m3 <fabian.ceressia@gmail.com>
2012-03-14 00:13:28 +0100

    - Add last Vlad change on the sound engine

--- Utopia/Sandbox/Sandbox.Client/Components/SandboxSoundManager.cs
+++ Utopia/Sandbox/Sandbox.Client/Components/SandboxSoundManager.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Utopia.Components;
+using S33M3CoreComponents.Cameras;
+using S33M3CoreComponents.Cameras.Interfaces;
 
 namespace Sandbox.Client.Components
 {
     public class SandboxSoundManager : SoundManager
     {
-        public SandboxSoundManager()
+        public SandboxSoundManager(CameraManager<ICameraFocused> cameraManager)
+            :base(cameraManager)
         {
             SetGuiButtonSound("Sounds\\Interface\\button_press.wav");
         }
