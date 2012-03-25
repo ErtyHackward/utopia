2d4e66faf2d4699c6a811ed7ee9aeccf504c564a
s33m3 <fabian.ceressia@gmail.com>
2012-03-25 23:24:39 +0200

    - Samll assembly glitch

--- Utopia/Sandbox/Sandbox.Client/Client.Binding.cs
+++ Utopia/Sandbox/Sandbox.Client/Client.Binding.cs
@@ -53,7 +53,7 @@ using Utopia.Action;
 using System.Collections.Generic;
 using System.Reflection;
 using S33M3CoreComponents.Debug;
-using S33M3_CoreComponents.Sprites;
+using S33M3CoreComponents.Sprites;
 using Utopia.Shared.Entities;
 using Sandbox.Shared;
 
