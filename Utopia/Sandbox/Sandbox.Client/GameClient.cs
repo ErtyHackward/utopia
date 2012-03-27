d839af60c727fc1d5eb9fd7438c2b3e1f22dc667
s33m3 <fabian.ceressia@gmail.com>
2012-03-27 13:47:28 +0200

    - Small debug component layout changes

--- Utopia/Sandbox/Sandbox.Client/GameClient.cs
+++ Utopia/Sandbox/Sandbox.Client/GameClient.cs
@@ -1,4 +1,4 @@
-﻿//#define SINGLEPLAYERSTART
+﻿#define SINGLEPLAYERSTART
 
 using System;
 using System.IO;
@@ -104,7 +104,7 @@ namespace Sandbox.Client
 
             //ClientSettings.Current.Settings.Login = vars.Login;
             //ClientSettings.Current.Save();
-            stateManager.ActivateGameStateAsync("MainMenu");
+            stateManager.ActivateGameStateAsync("LoadingGame");
 #else
             stateManager.ActivateGameStateAsync("Login");
 #endif
