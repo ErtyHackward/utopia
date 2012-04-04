556fd0d91e840a76f742f192f1785a3bce85ff1c
s33m3 <fabian.ceressia@gmail.com>
2012-04-04 22:06:35 +0200

    On going ...

--- Utopia/Sandbox/Sandbox.Client/Components/GUI/Settings/KeyBindingSettingsPanel.cs
+++ Utopia/Sandbox/Sandbox.Client/Components/GUI/Settings/KeyBindingSettingsPanel.cs
@@ -6,6 +6,7 @@ using S33M3CoreComponents.GUI.Nuclex.Controls;
 using S33M3DXEngine;
 using SharpDX.Direct3D11;
 using S33M3CoreComponents.GUI.Nuclex;
+using S33M3CoreComponents.Unsafe;
 
 namespace Sandbox.Client.Components.GUI.Settings
 {
@@ -14,6 +15,7 @@ namespace Sandbox.Client.Components.GUI.Settings
         #region Private Variables
         private SettingsComponent _parent;
         private string _panelName;
+        private D3DEngine _engine;
         #endregion
 
         #region Public Variables
@@ -21,13 +23,53 @@ namespace Sandbox.Client.Components.GUI.Settings
 
         public KeyBindingSettingsPanel(SettingsComponent parent, D3DEngine engine, UniRectangle bound)
         {
-            engine.ViewPort_Updated += engine_ViewPort_Updated;
+            _engine = engine;
+            _engine.ViewPort_Updated += engine_ViewPort_Updated;
             this.CanBeRendered = false;
             _panelName = "Key Bindings";
             _parent = parent;
             this.Bounds = bound;
 
+            _engine.GameWindow.KeyDown += new System.Windows.Forms.KeyEventHandler(GameWindow_KeyDown);
+
             InitializeComponent();
+        }
+
+        public override void Dispose()
+        {
+            _engine.GameWindow.KeyDown -= new System.Windows.Forms.KeyEventHandler(GameWindow_KeyDown);
+
+            base.Dispose();
+        }
+
+        void GameWindow_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            //Get selected cell
+            bool isLShift, isRShift, isLControl, isRControl;
+            string ModifierStr = string.Empty;
+            isLShift = (UnsafeNativeMethods.GetKeyState(0xA0) & 0x80) != 0; // VK_LSHIFT    
+            isRShift = (UnsafeNativeMethods.GetKeyState(0xA1) & 0x80) != 0; // VK_RSHIFT        
+            isLControl = (UnsafeNativeMethods.GetKeyState(162) & 0x80) != 0; // VK_LCONTROL              
+            isRControl = (UnsafeNativeMethods.GetKeyState(0xA3) & 0x80) != 0; // VK_RCONTROL              
+
+            if (isLShift) ModifierStr += ModifierStr.Length > 0 ? "+ LShiftKey" : "LShiftKey";
+            if (isRShift) ModifierStr += ModifierStr.Length > 0 ? "+ RShiftKey" : "RShiftKey";
+            if (isLControl) ModifierStr += ModifierStr.Length > 0 ? "+ LControlKey" : "LControlKey";
+            if (isRControl) ModifierStr += ModifierStr.Length > 0 ? "+ RControlKey" : "RControlKey";
+
+            string Keypressed = e.KeyData.ToString();
+
+            //If it is only a Modifier key pressed
+            if (Keypressed.Contains("Key"))
+            {
+                Console.WriteLine(ModifierStr);
+            }
+            else
+            {
+                Keypressed = Keypressed.Split(',')[0];
+                if (ModifierStr.Length > 0) ModifierStr = " + " + ModifierStr;
+                Console.WriteLine(Keypressed + ModifierStr);
+            }
         }
 
         void engine_ViewPort_Updated(Viewport viewport, Texture2DDescription newBackBuffer)
