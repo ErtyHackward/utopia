96b90ea495393b8950e6f13ade830648ee502318
Vladislav Pozdnyakov <hackward@gmail.com>
2012-04-24 16:44:24 +0700

    - soundmanager: custom ambient sounds, pre-load of sounds

--- Utopia/Sandbox/Sandbox.Client/Components/SandboxGameSoundManager.cs
+++ Utopia/Sandbox/Sandbox.Client/Components/SandboxGameSoundManager.cs
@@ -20,26 +20,30 @@ namespace Sandbox.Client.Components
             : base(soundEngine, cameraManager, singleArray, dynamicEntityManager, player)
         {
 
-            AddStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass01.ogg");
-            //AddStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass02.ogg");
-            //AddStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass03.ogg");
-            //AddStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass04.ogg");
+            // steps
+            RegisterStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass01.ogg");
+            //RegisterStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass02.ogg");
+            //RegisterStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass03.ogg");
+            //RegisterStepSound(CubeId.Grass, "Sounds\\Footsteps\\footsteps_grass04.ogg");
 
-            AddStepSound(CubeId.Sand, "Sounds\\Footsteps\\footsteps_sand01.ogg");
-            AddStepSound(CubeId.Sand, "Sounds\\Footsteps\\footsteps_sand02.ogg");
-            AddStepSound(CubeId.Sand, "Sounds\\Footsteps\\footsteps_sand03.ogg");
+            RegisterStepSound(CubeId.Sand, "Sounds\\Footsteps\\footsteps_sand01.ogg");
+            RegisterStepSound(CubeId.Sand, "Sounds\\Footsteps\\footsteps_sand02.ogg");
+            RegisterStepSound(CubeId.Sand, "Sounds\\Footsteps\\footsteps_sand03.ogg");
 
-            AddStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt01.ogg");
-            AddStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt02.ogg");
-            AddStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt03.ogg");
-            AddStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt04.ogg");
+            RegisterStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt01.ogg");
+            RegisterStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt02.ogg");
+            RegisterStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt03.ogg");
+            RegisterStepSound(CubeId.Dirt, "Sounds\\Footsteps\\footsteps_dirt04.ogg");
 
-            AddStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone01.ogg");
-            AddStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone02.ogg");
-            AddStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone03.ogg");
-            AddStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone04.ogg");
+            RegisterStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone01.ogg");
+            RegisterStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone02.ogg");
+            RegisterStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone03.ogg");
+            RegisterStepSound(CubeId.Stone, "Sounds\\Footsteps\\footsteps_stone04.ogg");
 
-            AddStepSound(CubeId.Water, "Sounds\\Footsteps\\footsteps_water01.ogg");
+            RegisterStepSound(CubeId.Water, "Sounds\\Footsteps\\footsteps_water01.ogg");
+
+            // ambiance
+            RegisterCubeAmbientSound(CubeId.Water, "Sounds\\Ambiance\\water_stream.ogg");
 
         }
     }
