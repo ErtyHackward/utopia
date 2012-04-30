a552d69584e427c7e37d5a96edd4dd35c0db1e9a
s33m3 <s33m3@localhost>
2012-04-30 21:24:28 +0200

    - Small Ajustement

---
+++ S33M3Engines/S33M3CoreComponents/States/SwitchComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using S33M3CoreComponents.States.Interfaces;
+using S33M3DXEngine.Main;
+
+namespace S33M3CoreComponents.States
+{
+    public abstract class SwitchComponent : DrawableGameComponent, ISwitchComponent
+    {
+        public abstract event EventHandler SwitchMoment;
+        public abstract event EventHandler EffectComplete;
+
+        public SwitchComponent()
+            : base()
+        {
+            this.IsSystemComponent = true;
+        }
+
+        public abstract void BeginSwitch();
+        public abstract void FinishSwitch();
+    }
+}
