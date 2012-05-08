54fcb6df3d8c6f06c784dbfbefb8f97f360b9702
s33m3 <fabian.ceressia@gmail.com>
2012-05-08 12:30:31 +0200

    - Missing file

---
+++ Utopia/Utopia.Shared/World/Processors/Utopia/LandformFct/UnderGrounds/UnderGround.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using S33M3CoreComponents.Noise;
+using S33M3CoreComponents.Noise.Fractal;
+using S33M3CoreComponents.Noise.Generator;
+using S33M3CoreComponents.Noise.ResultModifier;
+using S33M3CoreComponents.Noise.DomainModifier;
+using S33M3CoreComponents.Noise.Various;
+using S33M3CoreComponents.Noise.NoiseResultCombiner;
+
+namespace Utopia.Shared.World.Processors.Utopia.LandformFct
+{
+    public class UnderGround : ITerrainGenerator
+    {
+        #region Private Variables
+        private INoise _groundGradient;
+        private Gradient _groundGradientTyped;
+        private int _seed;
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        public UnderGround(int seed, Gradient groundGradient)
+            : this(seed, groundGradient, groundGradient)
+        {
+        }
+
+        public UnderGround(int seed, Cache<Gradient> groundGradient)
+            : this(seed, groundGradient, groundGradient.Source)
+        {
+        }
+
+        private UnderGround(int seed, INoise groundGradient, Gradient groundGradientTyped)
+        {
+            _groundGradient = groundGradient;
+            _groundGradientTyped = groundGradientTyped;
+            _seed = seed;
+        }
+
+        #region Public Methods
+        public INoise GetLandFormFct()
+        {
+            //REM
+            //The various parameters value here are scaled for a gradient being feed by 0 to 1 input value.
+            //When this gradient is configured to recevied other value range then some parameters needs to be rescaled
+            //That's the reason for using this _groundGradientTyped.AdjustY value
+            //This way no matter the the Gradient Range, the values impacting it will be rescaled.
+
+            //Create the Lowland base fractal with range from 0 to 1 values
+            INoise underground_fractal = new FractalRidgedMulti(new Simplex(_seed), 1, 3, enuBaseNoiseRange.ZeroToOne);
+
+            return underground_fractal;
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+
+    }
+}
