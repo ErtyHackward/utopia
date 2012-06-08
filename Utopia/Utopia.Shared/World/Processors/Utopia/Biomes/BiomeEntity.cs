cc72cc2bcd4ee28495baf11bfda961f87606a6b7
s33m3 <fabian.ceressia@gmail.com>
2012-06-08 16:30:47 +0200

    - Update new/deleted files

---
+++ Utopia/Utopia.Shared/World/Processors/Utopia/Biomes/BiomeEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utopia.Shared.World.Processors.Utopia.Biomes
+{
+    public class BiomeEntity
+    {
+        public static BiomeEntity None = new BiomeEntity() { EntityId = 0, ChanceOfSpawning = 0.0, EntityPerChunk = 0 };
+
+        public ushort EntityId;
+        public int EntityPerChunk;
+        public double ChanceOfSpawning;
+    }
+}
