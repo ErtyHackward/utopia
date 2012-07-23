8a45bbabcc18d8b8cb213ed0fa9bfcee5045bec1
s33m3 <fabian.ceressia@gmail.com>
2012-07-23 15:49:24 +0200

    - Splitted PlayerEntityManager

---
+++ Utopia/Utopia/Entities/Managers/PlayerEntityManager.Input.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utopia.Action;
+using Utopia.Shared.Entities;
+using SharpDX;
+using S33M3CoreComponents.Maths;
+
+namespace Utopia.Entities.Managers
+{
+    //Handle all Input related stuff for player
+    public partial class PlayerEntityManager
+    {
+        #region Private Variables
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Handle Player Actions - Movement and rotation input are not handled here
+        /// </summary>
+        private void inputHandler()
+        {
+            if (_inputsManager.ActionsManager.isTriggered(UtopiaActions.Move_Mode, CatchExclusiveAction))
+            {
+                if (Player.DisplacementMode == EntityDisplacementModes.Flying)
+                {
+                    DisplacementMode = EntityDisplacementModes.Walking;
+                }
+                else
+                {
+                    DisplacementMode = EntityDisplacementModes.Flying;
+                }
+            }
+
+            if (!HasMouseFocus) return; //the editor(s) can acquire the mouseFocus
+
+            if (_inputsManager.ActionsManager.isTriggered(UtopiaActions.Use_Left, CatchExclusiveAction))
+            {
+                if ((Player.EntityState.IsBlockPicked || Player.EntityState.IsEntityPicked) && Player.Equipment.LeftTool != null)
+                {
+                    //sends the client server event that does tool.use on server
+                    Player.LeftToolUse(ToolUseMode.LeftMouse);
+
+                    //client invocation to keep the client inventory in synch
+                    Player.Equipment.LeftTool.Use(Player, ToolUseMode.LeftMouse);
+                }
+            }
+
+            if (_inputsManager.ActionsManager.isTriggered(UtopiaActions.Use_Right, CatchExclusiveAction))
+            {
+                if ((Player.EntityState.IsBlockPicked || Player.EntityState.IsEntityPicked) && Player.Equipment.LeftTool != null)
+                {
+                    //Avoid the player to add a block where he is located !            
+                    BoundingBox playerPotentialNewBlock;
+                    ComputeBlockBoundingBox(ref Player.EntityState.NewBlockPosition, out playerPotentialNewBlock);
+
+                    if (!MBoundingBox.Intersects(ref VisualEntity.WorldBBox, ref playerPotentialNewBlock))
+                    {
+                        //sends the client server event that does tool.use on server
+                        Player.LeftToolUse(ToolUseMode.RightMouse);
+
+                        //client invocation to keep the client inventory in synch
+                        Player.Equipment.LeftTool.Use(Player, ToolUseMode.RightMouse);
+                    }
+                }
+            }
+
+            if (_inputsManager.ActionsManager.isTriggered(UtopiaActions.EntityUse, CatchExclusiveAction))
+            {
+                //TODO implement use 'picked' entity (picked here means entity is in world having cursor over it, not in your hand or pocket) 
+                //like opening a chest or a door  
+            }
+
+            if (_inputsManager.ActionsManager.isTriggered(UtopiaActions.EntityThrow, CatchExclusiveAction))
+            {
+                //TODO unequip left item and throw it on the ground, (version 0 = place it at newCubeplace, animation later)                
+                // and next, throw the right tool if left tool is already thrown
+            }
+
+        }
+        #endregion
+
+        
+    }
+}
---
+++ Utopia/Utopia/Entities/Managers/PlayerEntityManager.Move.Physic.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using S33M3Resources.Structs;
+using S33M3DXEngine.Main;
+using Utopia.Shared.Settings;
+using Utopia.Shared.Entities;
+
+namespace Utopia.Entities.Managers
+{
+    public partial class PlayerEntityManager
+    {
+        #region Private Variables
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        private void PhysicOnEntity(EntityDisplacementModes mode, ref GameTime timeSpent)
+        {
+            if (_stopMovedAction && _groundCubeProgile.SlidingValue == 0)
+            {
+                _stopMovedAction = false;
+            }
+
+            switch (mode)
+            {
+                case EntityDisplacementModes.Flying:
+                    _physicSimu.Friction = 0f;
+                    break;
+                case EntityDisplacementModes.Swiming:
+                    _physicSimu.Friction = 0.3f;
+                    PhysicSimulation(ref timeSpent);    //Apply physic constraint on new compute location
+                    break;
+                case EntityDisplacementModes.Walking:
+                    if (_physicSimu.OnGround)
+                    {
+                        if (_stopMovedAction == false)
+                        {
+                            _physicSimu.Friction = _groundCubeProgile.Friction; //0.25f;
+                        }
+                        else
+                        {
+                            //I did stop to move, but I'm on a sliding block => Will slide a little before ending movement
+                            _physicSimu.Friction = _groundCubeProgile.SlidingValue;
+                        }
+                    }
+                    else
+                    {
+                        _physicSimu.Friction = 0f;
+                    }
+                    PhysicSimulation(ref timeSpent);    //Apply physic constraint on new compute location
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Apply physic constraint on the new player location (Collisions tests, ...)
+        /// </summary>
+        /// <param name="timeSpent"></param>
+        private void PhysicSimulation(ref GameTime timeSpent)
+        {
+            Vector3I GroundDirection = new Vector3I(0, -1, 0);
+            Vector3D newWorldPosition;
+            double BlockOffset;
+
+            _cubesHolder.GetNextSolidBlockToPlayer(ref VisualEntity.WorldBBox, ref GroundDirection, out _groundCube);
+            //Half cube below me ??
+            _groundCubeProgile = GameSystemSettings.Current.Settings.CubesProfile[_groundCube.Cube.Id];
+            BlockOffset = _groundCubeProgile.YBlockOffset;
+            _groundBelowEntity = _groundCube.Position.Y + (1 - BlockOffset);
+            PlayerOnOffsettedBlock = (float)BlockOffset;//BlockOffset != 0;
+
+            _physicSimu.Simulate(ref timeSpent, out newWorldPosition);
+            _worldPosition.Value = newWorldPosition;
+
+            //After physic simulation, I'm still in the air ?
+            if (_worldPosition.Value.Y > _groundBelowEntity)
+            {
+                _physicSimu.OnGround = false;
+            }
+        }
+        #endregion
+
+    }
+}
