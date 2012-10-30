450c2df0617345609c00a5e7eceff9cc5368edfc
s33m3 <fabian.ceressia@gmail.com>
2012-10-30 17:05:12 +0100

    - World Height + Utopia height generator implemented. ps : Best result for utopia generator with 128 height, but the world height go up to 256 !

--- Utopia/Utopia.Shared/Structs/helpers/BlockHelper.cs
+++ Utopia/Utopia.Shared/Structs/helpers/BlockHelper.cs
@@ -1,29 +1,28 @@
 using S33M3Resources.Structs;
+using Utopia.Shared.Chunks;
 
 namespace Utopia.Shared.Structs.Helpers
 {
     public static class BlockHelper
     {
-        public static Vector3I ChunkSize = new Vector3I(16, 128, 16);
-
         public static Vector2I BlockToChunkPosition(Vector3I blockPosition)
         {
-            var vec2 = new Vector2I(blockPosition.X / ChunkSize.X, blockPosition.Z / ChunkSize.Z);
-            if (blockPosition.X < 0 && blockPosition.X % ChunkSize.X != 0) vec2.X--;
-            if (blockPosition.Y < 0 && blockPosition.Y % ChunkSize.Z != 0) vec2.Y--;
+            var vec2 = new Vector2I(blockPosition.X / AbstractChunk.ChunkSize.X, blockPosition.Z / AbstractChunk.ChunkSize.Z);
+            if (blockPosition.X < 0 && blockPosition.X % AbstractChunk.ChunkSize.X != 0) vec2.X--;
+            if (blockPosition.Y < 0 && blockPosition.Y % AbstractChunk.ChunkSize.Z != 0) vec2.Y--;
             return vec2;
         }
 
         public static Vector3I GlobalToInternalChunkPosition(Vector3I globalPosition)
         {
-            var vec3 = new Vector3I(globalPosition.X % ChunkSize.X, globalPosition.Y % ChunkSize.Y, globalPosition.Z % ChunkSize.Z);
+            var vec3 = new Vector3I(globalPosition.X % AbstractChunk.ChunkSize.X, globalPosition.Y % AbstractChunk.ChunkSize.Y, globalPosition.Z % AbstractChunk.ChunkSize.Z);
 
             if (vec3.X < 0)
-                vec3.X = ChunkSize.X + vec3.X;
+                vec3.X = AbstractChunk.ChunkSize.X + vec3.X;
             if (vec3.Y < 0)
-                vec3.Y = ChunkSize.Y + vec3.Y;
+                vec3.Y = AbstractChunk.ChunkSize.Y + vec3.Y;
             if (vec3.Z < 0)
-                vec3.Z = ChunkSize.Z + vec3.Z;
+                vec3.Z = AbstractChunk.ChunkSize.Z + vec3.Z;
 
             return vec3;
         }
@@ -35,8 +34,8 @@ namespace Utopia.Shared.Structs.Helpers
         /// <param name="blocksPositions"></param>
         public static void ConvertToGlobal(Vector2I chunkPosition, Vector3I[] blocksPositions)
         {
-            var dx = chunkPosition.X * ChunkSize.X;
-            var dz = chunkPosition.Y * ChunkSize.Z;
+            var dx = chunkPosition.X * AbstractChunk.ChunkSize.X;
+            var dz = chunkPosition.Y * AbstractChunk.ChunkSize.Z;
             for (int i = 0; i < blocksPositions.Length; i++)
             {
                 blocksPositions[i].X += dx;
