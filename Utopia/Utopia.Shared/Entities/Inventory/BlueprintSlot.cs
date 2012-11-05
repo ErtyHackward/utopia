0658267072552c16c81cb36d198963fb4b63493d
Vladislav Pozdnyakov <hackward@gmail.com>
2012-11-05 21:17:34 +0700

    - before using dx in the RealmEditor

---
+++ Utopia/Utopia.Shared/Entities/Inventory/BlueprintSlot.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace Utopia.Shared.Entities.Inventory
+{
+    /// <summary>
+    /// Represents a slot containing an entity(ies) from a blueprint
+    /// Should be used to initialize containers
+    /// </summary>
+    public class BlueprintSlot : ContainedSlot
+    {
+        public ushort BlueprintId { get; set; }
+
+        // overriding items and do whole writing by ourself
+        public override void Save(BinaryWriter writer)
+        {
+            // saving toolbar slot position
+            writer.Write(GridPosition);
+
+            // saving values
+            writer.Write(ItemsCount);
+            writer.Write(BlueprintId);
+        }
+
+        public override void LoadSlot(BinaryReader reader, EntityFactory factory)
+        {
+            // reading toolbar slot position
+            GridPosition = reader.ReadVector2I();
+
+            ItemsCount = reader.ReadInt32();
+            BlueprintId = reader.ReadUInt16();
+        }
+    }
+}
---
+++ Utopia/Utopia.Shared/Tools/EntityListForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Utopia.Shared.Tools
+{
+    partial class EntityListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.containerEditor1 = new Utopia.Shared.Tools.ContainerEditor();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // containerEditor1
+            // 
+            this.containerEditor1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.containerEditor1.Content = null;
+            this.containerEditor1.Icons = null;
+            this.containerEditor1.Location = new System.Drawing.Point(13, 13);
+            this.containerEditor1.Name = "containerEditor1";
+            this.containerEditor1.Size = new System.Drawing.Size(536, 208);
+            this.containerEditor1.TabIndex = 0;
+            this.containerEditor1.Text = "containerEditor1";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(474, 227);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApply.Location = new System.Drawing.Point(393, 227);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(75, 23);
+            this.buttonApply.TabIndex = 2;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            // 
+            // EntityListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 262);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.containerEditor1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EntityListForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Edit the contents";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ContainerEditor containerEditor1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonApply;
+    }
+}
