7b1b2dc3eb3fc8a95ba4a6b91336edf1acfa92e5
s33m3 <fabian.ceressia@gmail.com>
2012-11-13 15:04:30 +0100

    - Little bug fix

--- Utopia/Utopia.Shared/Entities/Concrete/OrientedCubePlaceableItem.cs
+++ Utopia/Utopia.Shared/Entities/Concrete/OrientedCubePlaceableItem.cs
@@ -57,14 +57,14 @@ namespace Utopia.Shared.Entities.Concrete
             else if (vector.Y == -1) //PUT on cube Bottom = (Ceiling)
             {
                 cubeEntity.Position = new Vector3D(owner.EntityState.PickedBlockPosition.X + 0.5f,
-                                   owner.EntityState.PickedBlockPosition.Y,
+                                   owner.EntityState.PickedBlockPosition.Y - 1f,
                                    owner.EntityState.PickedBlockPosition.Z + 0.5f);
                 double entityRotation;
 
                 if (Math.Abs(playerRotation.Z) >= Math.Abs(playerRotation.X))
                 {
-                    if (playerRotation.Z < 0) entityRotation = 0; //  result = "North";
-                    else entityRotation = MathHelper.Pi;
+                    if (playerRotation.Z < 0) entityRotation = MathHelper.Pi; //  result = "North";
+                    else entityRotation = 0;
                 }
                 else
                 {
@@ -73,7 +73,6 @@ namespace Utopia.Shared.Entities.Concrete
                 }
 
                 cubeEntity.Rotation = Quaternion.RotationAxis(new Vector3(0, 1, 0), (float)entityRotation);
-                cubeEntity.Rotation *= Quaternion.RotationAxis(new Vector3(1, 0, 0), MathHelper.Pi);
             }
             else //Put on a side
             {
