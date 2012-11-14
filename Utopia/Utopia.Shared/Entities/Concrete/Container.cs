dd45cc47f0522b1c9b0c22455d9573fde0f0ab9e
Vladislav Pozdnyakov <hackward@gmail.com>
2012-11-14 20:27:40 +0700

    - make Container to be OrientedCubePlaceableItem, - allow to delete entity from the configuration

--- Utopia/Utopia.Shared/Entities/Concrete/Container.cs
+++ Utopia/Utopia.Shared/Entities/Concrete/Container.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Utopia.Shared.Entities.Inventory;
+﻿using Utopia.Shared.Entities.Inventory;
 
 namespace Utopia.Shared.Entities.Concrete
 {
     /// <summary>
     /// Represents a container block entity
     /// </summary>
-    public class Container : CubePlaceableItem
+    public class Container : OrientedCubePlaceableItem
     {
         readonly SlotContainer<ContainedSlot> _content;
 
