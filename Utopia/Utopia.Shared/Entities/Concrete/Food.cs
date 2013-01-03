a49ee90448a39c4717bc8d65453e670899c97373
Vladislav Pozdnyakov <hackward@gmail.com>
2013-01-03 18:56:23 +0700

    - ghosted entity, almost done

--- Utopia/Utopia.Shared/Entities/Concrete/Food.cs
+++ Utopia/Utopia.Shared/Entities/Concrete/Food.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using S33M3Resources.Structs;
 using Utopia.Shared.Entities.Inventory;
 
 namespace Utopia.Shared.Entities.Concrete
@@ -14,6 +15,20 @@ namespace Utopia.Shared.Entities.Concrete
         public override ushort ClassId
         {
             get { return EntityClassId.Food; }
+        }
+
+        public override EntityPosition GetPosition(Interfaces.IDynamicEntity owner)
+        {
+            var pos = new EntityPosition();
+
+            // allow to put only on top of the entity
+            if (owner.EntityState.PickPointNormal.Y != 1)
+                return pos;
+
+            pos.Position = new Vector3D(owner.EntityState.PickPoint);
+            pos.Valid = true;
+
+            return pos;
         }
     }
 }
