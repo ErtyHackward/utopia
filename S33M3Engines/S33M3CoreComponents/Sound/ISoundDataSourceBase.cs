4e9f2c55ea2543069d454580e9bf58ea0d9e9c1a
s33m3 <s33m3@localhost>
2013-01-14 14:10:44 +0100

    - Adding new files

---
+++ S33M3Engines/S33M3CoreComponents/Sound/ISoundDataSourceBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace S33M3CoreComponents.Sound
+{
+    public interface ISoundDataSourceBase
+    {
+        SourceCategory Category { get; set; }
+        string FilePath { get; set; }
+        string Alias { get; set; }
+        float Volume { get; set; }
+        float Power { get; set; }
+        bool isStreamed { get; set; }
+    }
+}
---
+++ Utopia/Utopia.Shared/Tools/FullSoundSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using S33M3CoreComponents.Sound;
+
+namespace Utopia.Shared.Tools
+{
+    public class FullSoundSelector : ExpandableObjectConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string)) return false;
+            return true;
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            return Activator.CreateInstance(Type.GetType((string)value));
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            ISoundDataSourceBase v = value as ISoundDataSourceBase;
+            if (v.FilePath == null) return "";
+            return v.FilePath;
+        }
+    }
+}
