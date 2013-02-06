cf7e17664e94dc5133869fcf6477e01d42cf947c
s33m3 <fabian.ceressia@gmail.com>
2013-02-06 11:22:33 +0100

    - Removed river

--- Utopia/Utopia.Shared/World/Processors/Utopia/ClimateFct/Moisture.cs
+++ Utopia/Utopia.Shared/World/Processors/Utopia/ClimateFct/Moisture.cs
@@ -33,18 +33,18 @@ namespace Utopia.Shared.World.Processors.Utopia.ClimateFct
         public INoise GetLandFormFct()            
         {
             //Create the Lowland base fractal with range from 0 to 1 values
-            //INoise Moisture_fractal = new FractalFbm(new Simplex(_seed), _octave, _freq, enuBaseNoiseRange.ZeroToOne);
-            //INoise ClampedValue = new Clamp(Moisture_fractal, 0, 1);
-            //INoise Moisture_fractal_biased = new Gain(ClampedValue, 0.6);
-            //INoise Moisture_fractal_Offset = new ScaleOffset(Moisture_fractal_biased, 1, 0.1);
-            //INoise ClampedValue2 = new Clamp(Moisture_fractal_Offset, 0, 1);
+            INoise Moisture_fractal = new FractalFbm(new Simplex(_seed), _octave, _freq, enuBaseNoiseRange.ZeroToOne);
+            INoise ClampedValue = new Clamp(Moisture_fractal, 0, 1);
+            INoise Moisture_fractal_biased = new Gain(ClampedValue, 0.6);
+            INoise Moisture_fractal_Offset = new ScaleOffset(Moisture_fractal_biased, 1, 0.1);
+            INoise ClampedValue2 = new Clamp(Moisture_fractal_Offset, 0, 1);
 
-            //return ClampedValue2;
+            return ClampedValue2;
 
-            INoise TempOffset = new ScaleOffset(new Voronoi2(_seed, _freq, 0.6, new ScaleOffset(new FractalFbm(new Perlin(12345), 3, 2), 0.4, 0.0)), 1.0, 1.0); // + 1
-            INoise TempScale = new ScaleOffset(TempOffset, 0.5, 0.0); // / 2
-            INoise ClampedValue = new Clamp(TempScale, 0, 1);
-            return ClampedValue;
+            //INoise TempOffset = new ScaleOffset(new Voronoi2(_seed, _freq, 0.6, new ScaleOffset(new FractalFbm(new Perlin(12345), 3, 2), 0.4, 0.0)), 1.0, 1.0); // + 1
+            //INoise TempScale = new ScaleOffset(TempOffset, 0.5, 0.0); // / 2
+            //INoise ClampedValue = new Clamp(TempScale, 0, 1);
+            //return ClampedValue;
         }
         #endregion
 
