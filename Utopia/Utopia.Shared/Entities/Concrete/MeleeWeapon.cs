efd771d77b02a6cb87aef1918b2a71dbff509391
Vladislav Pozdnyakov <hackward@gmail.com>
2014-03-09 00:43:50 +0700

    - MeleeWeapon, sword

---
+++ Utopia/Utopia.Shared/Entities/Concrete/MeleeWeapon.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using ProtoBuf;
+using Utopia.Shared.Entities.Dynamic;
+using Utopia.Shared.Entities.Interfaces;
+using Utopia.Shared.Entities.Inventory;
+
+namespace Utopia.Shared.Entities.Concrete
+{
+    [Description]
+    [ProtoContract]
+    public class MeleeWeapon : Item, ITool
+    {
+        [Description("Weapon damage")]
+        [ProtoMember(1)]
+        public int Damage { get; set; }
+
+        public MeleeWeapon()
+        {
+            RepeatedActionsAllowed = true;
+            IsPickable = true;
+        }
+
+        public IToolImpact Use(IDynamicEntity owner)
+        {
+            var impact = new EntityToolImpact();
+
+            if (!owner.EntityState.IsEntityPicked)
+            {
+                return impact;
+            }
+
+            var entity = owner.EntityState.PickedEntityLink.Resolve(EntityFactory) as CharacterEntity;
+
+            if (entity == null)
+            {
+                return impact;
+            }
+
+            entity.Health -= Damage;
+
+            impact.Success = true;
+            impact.EntityId = entity.DynamicId;
+
+            // TODO: play "hurt" sound
+
+            return impact;
+        }
+
+        public bool RepeatedActionsAllowed { get; set; }
+    }
+}
