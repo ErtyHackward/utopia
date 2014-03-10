347e8e9fc7712c7eaec3371f0a75962a0ea2e1f7
s33m3 <fabian.ceressia@gmail.com>
2014-03-11 00:41:31 +0100

    - Added radial blur for ghost shadder

--- Utopia/Resources/Shared.Resources/EffectsPacks/Default/PostEffects/GrayScale.hlsl
+++ Utopia/Resources/Shared.Resources/EffectsPacks/Default/PostEffects/GrayScale.hlsl
@@ -2,6 +2,13 @@
 // Constant Buffer Variables
 //--------------------------------------------------------------------------------------
 
+
+static const float2 Center = { 0.5, 0.5 }; ///center of the screen (could be any place)
+static const float BlurStart = 1.0f; /// blur offset
+static const float BlurWidth = -0.1; ///how big it should be
+static const int nsamples = 10;
+static const float sampleStrength = 2.2f;
+
 //--------------------------------------------------------------------------------------
 //Vertex shader Input
 struct VSInput
@@ -43,7 +50,23 @@ VSOutput VS(in VSInput input)
 //[PS ENTRY POINT]
 float4 PS(in VSOutput input) : SV_Target
 {
-	float4 backBufferColor = PostEffectBackBuffer.Sample(SamplerPostEffectBackBuffer, input.TexCoord);
-	backBufferColor.rgb = dot(backBufferColor.rgb, float3(0.3, 0.59, 0.11));
-	return backBufferColor;
+	float2 UV = input.TexCoord - Center;
+
+	float dist = clamp(length(UV) * sampleStrength, 0.0f, 1.0f);
+
+	float4 color = PostEffectBackBuffer.Sample(SamplerPostEffectBackBuffer, input.TexCoord);
+
+	float4 c = 0;
+	for (int i = 0; i < nsamples; i++) {
+		float scale = BlurStart + BlurWidth*(i / (float)(nsamples - 1));
+		c += PostEffectBackBuffer.Sample(SamplerPostEffectBackBuffer, UV * scale + Center);
+	}
+	c /= nsamples;
+
+	c = lerp(color, c, dist);
+
+	//float4 backBufferColor = PostEffectBackBuffer.Sample(SamplerPostEffectBackBuffer, input.TexCoord);
+	//backBufferColor.rgb = dot(backBufferColor.rgb, float3(0.3, 0.59, 0.11));
+	c.rgb = dot(c.rgb, float3(0.3, 0.59, 0.11));
+	return c;
 }
