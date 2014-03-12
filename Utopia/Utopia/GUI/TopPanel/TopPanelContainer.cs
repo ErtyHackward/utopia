c4e127ccaef1cfb433cf68be47e1caa1e2f6901e
s33m3 <fabian.ceressia@gmail.com>
2014-03-12 15:32:39 +0100

    - On going

--- Utopia/Utopia/GUI/TopPanel/TopPanelContainer.cs
+++ Utopia/Utopia/GUI/TopPanel/TopPanelContainer.cs
@@ -21,9 +21,7 @@ namespace Utopia.GUI.TopPanel
 
         //Child components
         private PanelControl _compassPanel;
-        private PanelControl _playerPanel;
-
-        private EnergyBar _life;
+        private PanelControl _energiesPanel;
         #endregion
 
         #region Public properties
@@ -41,7 +39,6 @@ namespace Utopia.GUI.TopPanel
 
         public override void BeforeDispose()
         {
-            _life.Dispose();
             _d3DEngine.ScreenSize_Updated -= ScreenSize_Updated;
             base.BeforeDispose();
         }
@@ -53,13 +50,19 @@ namespace Utopia.GUI.TopPanel
         private void CreateChildsComponents()
         {
             _compassPanel = ToDispose(new PanelControl() { Bounds = new UniRectangle(new UniScalar(1.0f, -150), 0, 150, 150), Color = new ByteColor(255,255,255,128) });
-            _playerPanel = ToDispose(new PanelControl() { Bounds = new UniRectangle(0, 0, 150, 75), Color = new ByteColor(255, 255, 255, 128) });
+            _energiesPanel = ToDispose(new PanelControl() { HidedPanel=true, Bounds = new UniRectangle(0, 0, 200, 150), Color = new ByteColor(255, 255, 255, 128) });
 
-            _life = new EnergyBar() { Bounds = new UniRectangle(5, 5, new UniScalar(1.0f, -10f), new UniScalar(0.33f, 0f)) };
-            _playerPanel.Children.Add(_life);
+            var life = new EnergyBar() { FrameName = "LifeEnergyBar", Bounds = new UniRectangle(5, 5, new UniScalar(1.0f, -10f), 35) };
+            _energiesPanel.Children.Add(life);
+
+            var lifeBar = new EnergyBar() { FrameName = "EnergyBar", Bounds = new UniRectangle(5 + 2, 5 + 9, new UniScalar(1.0f / 2.0f, -10f) - 24, 35 - 9), Color = new ByteColor(255, 40, 40, 255) };
+            _energiesPanel.Children.Add(lifeBar);
+
+            var air = new EnergyBar() { FrameName = "AirEnergyBar", Bounds = new UniRectangle(5, 40, new UniScalar(1.0f, -10f), 35) };
+            _energiesPanel.Children.Add(air);
 
             this.Children.Add(_compassPanel);
-            this.Children.Add(_playerPanel);
+            this.Children.Add(_energiesPanel);
         }
 
         private void ScreenSize_Updated(ViewportF viewport, SharpDX.Direct3D11.Texture2DDescription newBackBuffer)
