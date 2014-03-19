76e8e8a92afb6747f7053ebc93f60b7782238505
s33m3 <fabian.ceressia@gmail.com>
2014-03-19 17:30:40 +0100

    - Compass on going

---
+++ Utopia/Utopia/GUI/WindRose/CompassControl.cs
@@ -0,0 +1,13 @@
+﻿using S33M3CoreComponents.GUI.Nuclex.Controls;
+using S33M3CoreComponents.GUI.Nuclex.Controls.Arcade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utopia.GUI.WindRose
+{
+    public class CompassControl : PanelControl
+    {
+    }
+}
