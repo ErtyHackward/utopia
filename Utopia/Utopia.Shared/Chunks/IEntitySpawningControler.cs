d856b9cecf6771f19d64ad5287574ac5cdd26781
s33m3 <s33m3@localhost>
2014-04-02 14:27:08 +0200

    - Finish spawning location logic

--- Utopia/Utopia.Shared/Chunks/IEntitySpawningControler.cs
+++ Utopia/Utopia.Shared/Chunks/IEntitySpawningControler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using S33M3CoreComponents.Maths;
+using S33M3Resources.Structs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,6 @@ namespace Utopia.Shared.Chunks
 {
     public interface IEntitySpawningControler
     {
+        bool TryGetSpawnLocation(ChunkSpawnableEntity entity, AbstractChunk chunk, ByteChunkCursor cursor, FastRandom rnd, out Vector3D entitySpawnLocation);
     }
 }
