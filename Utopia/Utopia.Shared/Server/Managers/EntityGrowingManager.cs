267896f9b69250d411fe30c9b1742ba1407cbdb9
s33m3 <s33m3@localhost>
2014-04-10 10:26:11 +0200

    - Begin Adding EntityGrowingManager on server side

---
+++ Utopia/Utopia.Shared/Server/Managers/EntityGrowingManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utopia.Shared.Server.Managers
+{
+    public class EntityGrowingManager
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        #region Private variable
+        private ServerCore _server;
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        public EntityGrowingManager(ServerCore server)
+        {
+            _server = server;
+        }
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+    }
+}
