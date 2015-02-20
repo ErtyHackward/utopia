cc131c4bbc29c3be2ed9ef770296d2026397ff3c
Vladislav Pozdnyakov <hackward@gmail.com>
2015-02-20 11:29:39 +0700

    - change octree to support non-cube chunks

--- Utopia/Utopia.Shared/Chunks/OctreeChunkDataProvider.cs
+++ Utopia/Utopia.Shared/Chunks/OctreeChunkDataProvider.cs
@@ -9,12 +9,15 @@ namespace Utopia.Shared.Chunks
 {
     public class OctreeChunkDataProvider : ChunkDataProvider
     {
-        private readonly OctreeNode _rootNode = new OctreeNode();
+        private readonly OctreeNode _rootNode;
+        private Vector3I _chunkSize;
 
-        public override Vector3I ChunkSize {
-            get { return new Vector3I(16, 16, 16); }
-            set { throw new NotSupportedException(); } 
+        public override Vector3I ChunkSize
+        {
+            get { return _chunkSize; }
+            set { _chunkSize = value; }
         }
+
         public override ChunkColumnInfo[] ColumnsInfo { get; set; }
 
         public override ChunkColumnInfo GetColumnInfo(int inChunkPositionX, int inChunkPositionZ)
@@ -24,6 +27,12 @@ namespace Utopia.Shared.Chunks
 
         public override ChunkMetaData ChunkMetaData { get; set; }
 
+        public OctreeChunkDataProvider()
+        {
+            _rootNode = new OctreeNode();
+            _chunkSize = AbstractChunk.ChunkSize;
+        }
+
         public override byte[] GetBlocksBytes()
         {
             throw new NotImplementedException();
@@ -32,11 +41,12 @@ namespace Utopia.Shared.Chunks
         public override byte GetBlock(Vector3I position)
         {
             OctreeNode node = _rootNode;
-            int sizeFactor = 16;
+            var sizeFactor = ChunkSize;
             while (node.HasChilds)
             {
                 node = node.Childs[GetArrayIndexFromPos(position, sizeFactor)];
                 sizeFactor /= 2;
+                sizeFactor = Vector3I.Max(sizeFactor, Vector3I.One);
             }
             return node.Value;
         }
@@ -54,11 +64,12 @@ namespace Utopia.Shared.Chunks
         public override void SetBlock(Vector3I position, byte blockValue, BlockTag tag = null, uint sourceDynamicId = 0)
         {
             OctreeNode node = _rootNode;
-            int sizeFactor = 16;
+            var sizeFactor = ChunkSize;
             while (node.HasChilds)
             {
                 node = node.Childs[GetArrayIndexFromPos(position, sizeFactor)];
                 sizeFactor /= 2;
+                sizeFactor = Vector3I.Max(sizeFactor, Vector3I.One);
             }
 
             if (node.Value == blockValue)
@@ -70,11 +81,12 @@ namespace Utopia.Shared.Chunks
 
             var prevValue = node.Value;
 
-            while (sizeFactor >= 2)
+            while (sizeFactor.x >= 2 || sizeFactor.y >= 2 || sizeFactor.z >= 2)
             {
                 node.Split(prevValue);
                 node = node.Childs[GetArrayIndexFromPos(position, sizeFactor)];
                 sizeFactor /= 2;
+                sizeFactor = Vector3I.Max(sizeFactor, Vector3I.One);
                 needMerge = false;
             }
             
@@ -95,9 +107,10 @@ namespace Utopia.Shared.Chunks
                 node = node.Childs[GetArrayIndexFromPos(position, sizeFactor)];
                 inheritance.Add(node);
                 sizeFactor /= 2;
+                sizeFactor = Vector3I.Max(sizeFactor, Vector3I.One);
             }
 
-            for (int i = inheritance.Count - 1; i >= 0; i--)
+            for (int i = inheritance.Count - 2; i >= 0; i--)
             {
                 if (inheritance[i].Childs.All(n => !n.HasChilds && n.Value == blockValue))
                 {
@@ -123,11 +136,11 @@ namespace Utopia.Shared.Chunks
             get { throw new NotImplementedException(); }
         }
 
-        public int GetArrayIndexFromPos(Vector3I position, int sizeFactor)
+        public int GetArrayIndexFromPos(Vector3I position, Vector3I sizeFactor)
         {
-            return (position.x % sizeFactor >= sizeFactor / 2 ? 1 : 0) +
-                   (position.z % sizeFactor >= sizeFactor / 2 ? 2 : 0) +
-                   (position.y % sizeFactor >= sizeFactor / 2 ? 4 : 0);
+            return (position.x % sizeFactor.x >= sizeFactor.x / 2 ? 1 : 0) +
+                   (position.z % sizeFactor.z >= sizeFactor.z / 2 ? 2 : 0) +
+                   (position.y % sizeFactor.y >= sizeFactor.y / 2 ? 4 : 0);
         }
     }
 
